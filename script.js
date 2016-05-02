17ae6f97bc8d5b7fd7fd752095ca81dbef3d16dc
travisen <travis.nykaza@gmail.com>
2016-05-01 21:45:57 -0400

    basic movement working

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var body = document.body;
 
-var makeTable = function(){
+var position = 1;
+
+var table = function(){
 
 	tbl = document.createElement("table");
+	var cellId = 1; 
 
 	for (var row = 1; row <= 4; ++row) {	
-
 		var tRow =document.createElement("tr");
 		tbl.appendChild(tRow);
 
@@ -21,12 +23,13 @@ var makeTable = function(){
 				tempVal = tempVal - 1;
 				var tContent = document.createElement("td");
 				tContent.textContent = col + ", " + tempVal;
+				tContent.id = cellId;
+				++cellId;
 				tbl.appendChild(tContent);
 				tContent.style.border = "1px solid #000";
 			}
 		}
 	}
-	tbl.style.border = "1px solid #000"
 	body.appendChild(tbl);
 }
 
@@ -34,15 +37,36 @@ var makeButton = function(buttonName) {
 	var newButton = document.createElement("button");
 	newButton.textContent = buttonName;
 	newButton.id = buttonName;
-
 	body.appendChild(newButton);
 }
 
+var isSelected = function(cellId){
+	var on = true;
+	if (on == true){
+		document.getElementById(cellId).style.border = "2px solid #000";
+	}
+}
+
+var unselect = function(cellId){
+	document.getElementById(cellId).style.border = "1px solid #000";
+}
+
+var calcMovement = function(currentCellId){
+	position = currentCellId;
+	unselect(currentCellId);
+	position = position + 1;
+	isSelected(position);
+}
+
+table();
 // button id is same as button contents
-makeTable();
 makeButton("Up");
 makeButton("Down");
 makeButton("Left");
 makeButton("Right");
 makeButton("Mark");
 
+isSelected(1); // start at position 1
+//unselect(1);
+document.getElementById("Right").addEventListener("click", function(){
+ calcMovement(position);});
